ecd8c787ec4370d6a935c99dfb06a3d44a9e7aab
davidschwarz153 <schwarz.david100@gmail.com>
2025-02-06 13:38:29 +0100

    fixed bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import Cards from "./components/renderCards"
 
 function App() {
   return (
-    <>
-      <header className="flex flex-col justify-center items-center m-10">
-        <h1 className="text-5xl font-bold mb-5">ZooPartyDB</h1>
+    <main className="bg-gray-950">
+      <header className="flex flex-col justify-center items-center text-white">
+        <h1 className="text-5xl font-bold m-10 mb-5">ZooPartyDB</h1>
         <p className="text-xl font-bold">Explore the facinating world of zoo animals!</p>
       </header>
-      <section className="grid grid-cols-3 gap-12 m-10 justify-center content-center">
+      <section className="grid grid-cols-3 gap-12 m-10 justify-center content-center text-white">
         <Cards></Cards>
       </section>
-    </>
+    </main>
   )
 }
 
